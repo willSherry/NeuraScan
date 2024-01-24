dadd1bb21eec2381baf4ad5ac86354c74e024a79
Will <88620242+willSherry@users.noreply.github.com>
2024-01-24 23:08:30 +0000

    Improved image loading, set up testing and validation data, created CNN model with 99.12% accuracy.

--- main.py
+++ main.py
@@ -1,48 +1,99 @@
+import os
+import numpy
+import numpy as np
+import pandas as pd
+import imghdr
 import cv2
 import tensorflow as tf
-import os
-import imghdr
-from matplotlib import pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers, models
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, MaxPooling2D, Dense, Dropout, GlobalAveragePooling2D
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import optimizers, losses
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Setting GPU memory comsumption growth and avoiding out of memory error
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+import PIL
+from PIL import UnidentifiedImageError
+from pathlib import Path
+import os.path
 
+import itertools
+from sklearn.metrics import classification_report, confusion_matrix
+
+# Getting data -- MAY HAVE TO CHANGE THIS
 project_directory = os.getcwd()
+data_directory = os.path.join(project_directory, 'Data')
+image_directory = Path(data_directory)
 
-# Directory for the dataset
-data_dir = os.path.join(project_directory, 'Data')
+filepaths = list(image_directory.glob(r'**/*.JPG')) + list(image_directory.glob(r'**/*.jpg'))
+labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
 
-image_exts = ['jpeg', 'jpg', 'bmp', 'png']
+filepaths = pd.Series(filepaths, name='Filepath').astype(str)
+labels = pd.Series(labels, name='Label')
 
-# ModerateDementedData = os.path.join(data_dir, 'Moderate_Demented')
+image_df = pd.concat([filepaths, labels], axis=1)
+path = image_directory.glob("*.jpg")
+for img_p in path:
+    try:
+        img_p = PIL.Image.open(img_p)
+    except PIL.UnidentifiedImageError:
+        print(img_p)
 
-# Removing any dodgy images from the datasets
-for image_class in os.listdir(data_dir):
-    for image in os.listdir(os.path.join(data_dir, image_class)):
-        image_path = os.path.join(data_dir, image_class, image)
-        try:
-            img = cv2.imread(image_path)
-            tip = imghdr.what(image_path)
-            if tip not in image_exts:
-                print('Image not in ext list {}'.format(image_path))
-                os.remove(image_path)
-        except Exception as e:
-            print('Issue with image {}'.format(image_path))
-            os.remove(image_path)
+#Displaying 16 x 16 image of the data with labels
+random_index = np.random.randint(0, len(image_df), 16)
+fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(10, 10),
+                         subplot_kw={'xticks': [], 'yticks': []})
 
-data = tf.keras.utils.image_dataset_from_directory(data_dir, image_size=(128, 128))
-data_iterator = data.as_numpy_iterator()
-batch = data_iterator.next()
-# Images represented as numpy arrays
-batch[0].shape
-
-fig, ax = plt.subplots(ncols=4, figsize=(20,20))
-for idx, img in enumerate(batch[0][:4]):
-    ax[idx].imshow(img.astype(int))
-    ax[idx].title.set_text(batch[1][idx])
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(image_df.Filepath[random_index[i]]))
+    ax.set_title(image_df.Label[random_index[i]])
+plt.tight_layout()
 plt.show()
 
-3326
-# STILL HAVE TO COMMIT TO GITHUB
+# Setting validation split
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   validation_split=0.2)
+
+# Setting training data
+train_images = train_datagen.flow_from_directory(
+    data_directory,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+# Setting validation data
+validation_images = train_datagen.flow_from_directory(
+    data_directory,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# CNN Model
+model = tf.keras.models.Sequential([
+    Conv2D(16, (3,3), activation = 'relu', input_shape = (224,224, 3)),
+    MaxPooling2D(2,2),
+    Conv2D(32, (3,3), activation = 'relu'),
+    MaxPooling2D(2,2),
+    Conv2D(32, (3,3), activation = 'relu'),
+    MaxPooling2D(2,2),
+    Conv2D(32, (3,3), activation = 'relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(512, activation = 'relu'),
+    Dropout(0.2),
+    Dense(4, activation = 'softmax')
+])
+
+model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(), metrics = ['accuracy'])
+history = model.fit(train_images,
+    steps_per_epoch=len(train_images),
+    validation_data=validation_images,
+    validation_steps=len(validation_images),
+    epochs=10)
